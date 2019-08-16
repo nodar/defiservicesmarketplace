c7444978b8126e7ebb3d92b0d7f24024dad581aa
Nodar Janashia <nodar.janashia@gmail.com>
2019-08-16 10:32:27 -0400

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          DeFi Services Marketplaces
+          DeFi Services Marketplace
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
